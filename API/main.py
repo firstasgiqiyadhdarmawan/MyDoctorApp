1797f928ed1788d7ff6240ccddd9a94931174297
Vanz-Noob <renaldivanza@gmail.com>
2022-12-28 18:33:10 +0700

    feat: fix data path

--- API/main.py
+++ API/main.py
@@ -10,21 +10,21 @@ def hello():
 
 @app.route('/getHoax', methods = ['GET'])
 def getHoax():
-    with open ('G:\TUGAS KULIAH\Semester 7\PemMob\MyDoctorApp\API\static\data\hoax\hoax.json', encoding='utf-8') as f:
+    with open ('.\static\data\hoax\hoax.json', encoding='utf-8') as f:
         lines = json.loads(f.read())
 
     return jsonify(lines)
 
 @app.route('/getHospital', methods = ['GET'])
 def getHospital():
-    with open ('G:\TUGAS KULIAH\Semester 7\PemMob\MyDoctorApp\API\static\data\hospital\hospital.json', encoding='utf-8') as f:
+    with open ('.\static\data\hospital\hospital.json', encoding='utf-8') as f:
         lines = json.loads(f.read())
 
     return jsonify(lines)
 
 @app.route('/getNews', methods = ['GET'])
 def getNews():
-    with open ('G:\TUGAS KULIAH\Semester 7\PemMob\MyDoctorApp\API\static\data\zNews\zNews.json', encoding='utf-8') as f:
+    with open ('.static\data\zNews\zNews.json', encoding='utf-8') as f:
         lines = json.loads(f.read())
 
     return jsonify(lines)
