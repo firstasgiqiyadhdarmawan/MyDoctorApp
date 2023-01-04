1baf8f0793a415c07f254411b8f0edd35ddd55da
Vanz-Noob <renaldivanza@gmail.com>
2023-01-04 21:27:08 +0700

    feat: adding register route

--- API/main.py
+++ API/main.py
@@ -61,7 +61,75 @@ def getStats():
         lines = json.loads(f.read())
 
     return jsonify(lines)
- 
+
+#register user + initialiazing kategori
+@app.route("/register",methods=["POST", "GET"])
+def register():
+    try:
+        request_data = request.get_json()
+        email = request_data['email']
+        password = request_data['password']
+        pekerjaan = request_data['pekerjaan']
+        full_name = request_data['full_name']
+        Hpassword = sha256_crypt.encrypt(password)   
+        
+        #connect database
+        if os.environ.get('GAE_ENV') == 'standard':
+            unix_socket = '/cloudsql/{}'.format(db_connection_name)
+            cnx = pymysql.connect(user=db_user, password=db_password,
+                                unix_socket=unix_socket, db=db_name)
+        else:
+            host = '127.0.0.1'
+            cnx = pymysql.connect(user=db_user, password=db_password,
+                                host=host, db=db_name)
+        #validation
+        # password
+        if not re.fullmatch(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$', password):
+            return jsonify(
+                {
+                    'message': 'password character must be atleast 8 character with capital case and number character'
+                }
+            ), 400
+
+        # email
+        emailformat = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+        if not re.fullmatch(emailformat, email):
+            return jsonify(
+                {
+                    'message': 'email is not in valid format'
+                }
+            ), 400
+        # validate if email or email is used
+        # exist = user_service.check_existing_user(email, email)
+        with cnx.cursor() as cursor:
+            cursor.execute('SELECT iduser FROM user WHERE LOWER(email) = LOWER(%s) OR LOWER(email) = LOWER(%s);', (email, email))
+            exist = cursor.fetchone()
+        cnx.close()
+        
+        if exist:
+            return jsonify(
+                {
+                    'message': 'user already exist'
+                }
+            ), 400
+
+        #querying sql
+        with cnx.cursor() as cursor:
+            cursor.execute('INSERT INTO user (email, full_name, password, pekerjaan) VALUES (%s, %s, %s, %s);', (email, full_name, Hpassword, pekerjaan))
+            cnx.commit()
+        cnx.close()
+
+        return jsonify({
+                "email": email,
+                "full_name" : full_name,
+                "pekerjaan" : pekerjaan,
+                "code": "sukses",
+            })
+    except Exception as e:
+        return jsonify({
+            "message": str(e)
+        })
+
 #login
 @app.route("/login",methods=["POST", "GET"])
 def login():
@@ -84,25 +152,12 @@ def login():
         if not user:
             return jsonify({'status': 'failed', 'message': 'no active user found'}),401
 
-        if not sha256_crypt.verify(password, user[6]):
+        if not sha256_crypt.verify(password, user[5]):
             return jsonify({'status': 'failed', 'message': 'either email or password is invalid'}),401
-        
-        # # generate new token
-        # expires = timedelta(days=1)
-        # expires_refresh = timedelta(days=3)
-        # identity = {
-        #     'id_user': user[0],
-        #     'email': user[5],
-        #     'status':user[9]
-        # }
 
-        # access_token = create_access_token(identity=identity, fresh=True, expires_delta=expires)
-        # refresh_token = create_refresh_token(identity=identity, expires_delta=expires_refresh)
         return jsonify(
             {
                 'status': 'success',
-                # 'access': access_token,
-                # 'refresh': refresh_token,
                 'user':{
                     'email': user[5],
                     'user_pic': user[7],
